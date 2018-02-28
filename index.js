43f07d30487df7933e3ded1b4578063efc956e44
Nathan Pownell <dirato2000@gmail.com>
2018-02-28 23:59:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+var recipes = new Object({prop: 1});
+function updateObjectWithKeyAndValue(object, key, value) {
+  return recipes
+  prop2[recipes] = 2
+  return recipes
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+function deleteFromObjectByKey(object, key){
+  var newRecipes = Object.assign({prop: 2}, recipes)
+  delete newRecipes.foo
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  
+}
